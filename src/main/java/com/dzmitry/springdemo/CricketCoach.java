270afdbdc03d4b709326f510b3daa94812367f88
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-07-03 20:14:34 +0200

    inject values

--- src/main/java/com/dzmitry/springdemo/CricketCoach.java
+++ src/main/java/com/dzmitry/springdemo/CricketCoach.java
@@ -5,12 +5,30 @@ import com.dzmitry.springdemo.service.FortuneService;
 public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
+    private String emailAddress;
+    private String team;
 
     public CricketCoach() {
     }
 
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
     }
 
     @Override
--- src/main/java/com/dzmitry/springdemo/SetterDemoApp.java
+++ src/main/java/com/dzmitry/springdemo/SetterDemoApp.java
@@ -8,11 +8,14 @@ public class SetterDemoApp {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Coach theCoach = context.getBean("myCricketCoach", Coach.class);
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         System.out.println(theCoach.getDailyWorkout());
 
         System.out.println(theCoach.getDailyFortune());
+
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
 
         context.close();
     }
