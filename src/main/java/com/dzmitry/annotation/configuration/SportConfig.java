64ecd4989757af440d53e77c0fd40bf2aabc1242
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-07-11 18:47:28 +0200

    config without xml

---
+++ src/main/java/com/dzmitry/annotation/configuration/SportConfig.java
@@ -0,0 +1,9 @@
+package com.dzmitry.annotation.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.dzmitry")
+public class SportConfig {
+}
